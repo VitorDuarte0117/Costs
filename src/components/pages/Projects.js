5be9aa400e42cb481d37c3739873f5c94605a88c
Vitor <vitorduarte.vds7@gmail.com>
2022-03-18 15:28:06 -0300

    added the feature of rescuing projects from database and CSS

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,7 @@
 import { useLocation } from "react-router-dom";
 
+import { useState, useEffect } from "react";
+
 import Message from "../layout/Message";
 
 import styles from "./Projects.module.css";
@@ -8,12 +10,30 @@ import Container from "../layout/Container";
 
 import LinkButton from "../layout/LinkButton";
 
+import ProjectCard from "../project/ProjectCard";
+
 function Projects() {
+    const [projects, setProjects] = useState([]);
+
     const location = useLocation();
     let message = "";
     if (location.state) {
         message = location.state.message;
     }
+
+    useEffect(() => {
+        fetch("http://localhost:5000/projects", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProjects(data);
+            })
+            .catch((err) => console.log(err));
+    }, []);
 
     return (
         <div className={styles.project_container}>
@@ -23,7 +43,16 @@ function Projects() {
             </div>
             {message && <Message msg={message} type="success" />}
             <Container customClass="start">
-                <p>Projects...</p>
+                {projects.length > 0 &&
+                    projects.map((project) => (
+                        <ProjectCard
+                            id={project.id}
+                            name={project.name}
+                            budget={project.budget}
+                            category={project.category.name}
+                            key={project.id}
+                        />
+                    ))}
             </Container>
         </div>
     );
