3243c351c409f8d53a0d1807da44417d007d6040
Vitor <vitorduarte.vds7@gmail.com>
2022-03-21 15:19:29 -0300

    concluded the edit feature and some adjustments

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,10 +3,17 @@ import styles from "./Project.module.css";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
+import Container from "../layout/Container";
+import Message from "../layout/Message";
+import ProjectForm from "../project/ProjectForm";
+
 function Project() {
     const { id } = useParams();
 
-    const [project, setProject] = useState();
+    const [project, setProject] = useState([]);
+    const [showProjectForm, setShowProjectForm] = useState(false);
+    const [message, setMessage] = useState();
+    const [type, setType] = useState();
 
     useEffect(() => {
         fetch(`http://localhost:5000/projects/${id}`, {
@@ -23,7 +30,82 @@ function Project() {
                 console.log(err);
             });
     }, [id]);
+    function editPost(project) {
+        // budget validation
+        if (project.budget < project.cost) {
+            setMessage(
+                "The budget could not be less than the cost of the project"
+            );
+            setType("error");
+            return false;
+        }
 
-    return <p>{project.name}</p>;
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(project),
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProject(data);
+                setShowProjectForm(false);
+                setMessage("Project updated successfully");
+                setType("success");
+            })
+            .catch((err) => console.log(err));
+    }
+
+    function toggleProjectForm() {
+        setShowProjectForm(!showProjectForm);
+    }
+
+    return (
+        <>
+            {project.name ? (
+                <div className={styles.project_details}>
+                    <Container customClass="column">
+                        {message && <Message type={type} msg={message} />}
+                        <div className={styles.details_container}>
+                            <h1>Project: {project.name}</h1>
+                            <button
+                                className={styles.btn}
+                                onClick={toggleProjectForm}
+                            >
+                                {!showProjectForm ? "Edit project" : "Close"}
+                            </button>
+                            {!showProjectForm ? (
+                                <div className={styles.project_info}>
+                                    <p>
+                                        <span>Category:</span>
+                                        {project.category.name}
+                                    </p>
+                                    <p>
+                                        <span>Total budget:</span>R$
+                                        {project.budget}
+                                    </p>
+                                    <p>
+                                        <span>Total used:</span>R${project.cost}
+                                    </p>
+                                </div>
+                            ) : (
+                                <div className={styles.project_info}>
+                                    <ProjectForm
+                                        handleSubmit={editPost}
+                                        btnText="Finish editing"
+                                        projectData={project}
+                                    />
+                                </div>
+                            )}
+                        </div>
+                    </Container>
+                </div>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </>
+    );
 }
+
 export default Project;
